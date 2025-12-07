19c33be7d4e3bdc33503af3b17d339d9c05bcdb1
DouglasFeliphe <douglasfeliphe@hotmail.com>
2025-12-07 11:45:20 -0300

    feat: add expo location

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import './global.css';
 
 import { DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { useColorScheme } from 'react-native';
-import { useMemo } from 'react';
+import { useColorScheme, View, Text } from 'react-native';
+import { useEffect, useMemo, useState } from 'react';
+
+import * as Location from 'expo-location';
 
 import 'react-native-gesture-handler';
 
@@ -12,5 +14,39 @@ export default function App() {
   const colorScheme = useColorScheme();
   const theme = useMemo(() => (colorScheme === 'dark' ? DarkTheme : DefaultTheme), [colorScheme]);
 
-  return <Navigation theme={theme} />;
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  console.log('location :', location);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = 'Waiting...';
+
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View>
+      <Text>{text}</Text>
+      <Text>{location?.coords.latitude}</Text>
+      <Text>{location?.coords.longitude}</Text>
+    </View>
+  );
+  // return <Navigation theme={theme} />;
 }
