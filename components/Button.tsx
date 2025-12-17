de138af1fda64386d7d6ae383bdbb0e797975e5a
DouglasFeliphe <douglasfeliphe@hotmail.com>
2025-12-17 07:20:16 -0300

    feat: loading on button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,18 @@
+import { EvilIcons, MaterialIcons } from '@expo/vector-icons';
 import { forwardRef } from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from 'react-native';
+import colors from 'tailwindcss/colors';
+import { Loader } from './Loader';
 
 type ButtonProps = {
   title: string;
+  isLoading?: boolean;
 } & TouchableOpacityProps;
 
 export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps }, ref) => {
@@ -11,7 +21,13 @@ export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps 
       ref={ref}
       {...touchableProps}
       className={`${styles.button} ${touchableProps.className}`}>
-      <Text className={styles.buttonText}>{title}</Text>
+      <View className={`${styles.wrapper}`}>
+        <Text className={styles.buttonText}>
+          {touchableProps.isLoading ? 'Carregando dados...' : title}
+        </Text>
+        {/* {!touchableProps.isLoading && <ActivityIndicator color="white" />} */}
+        {touchableProps.isLoading && <Loader />}
+      </View>
     </TouchableOpacity>
   );
 });
@@ -19,6 +35,7 @@ export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps 
 Button.displayName = 'Button';
 
 const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
+  button: 'h-12 flex justify-center bg-indigo-500 rounded-[28px] shadow-md',
   buttonText: 'text-white text-lg font-semibold text-center',
+  wrapper: 'flex-row items-center justify-center ',
 };
