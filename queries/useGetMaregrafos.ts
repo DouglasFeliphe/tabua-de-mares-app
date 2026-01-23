e44cbdfe6242fac87f591ab1c25c7074e8167ad5
DouglasFeliphe <douglasfeliphe@hotmail.com>
2026-01-23 15:08:15 -0300

    refactor: update useGetMareGrafosQuery to accept isEnabled prop for conditional querying

--- queries/useGetMaregrafos.ts
+++ queries/useGetMaregrafos.ts
@@ -2,13 +2,15 @@ import { QUERY_KEYS } from './../consts/queryKeys';
 import { useQuery } from '@tanstack/react-query';
 import { getMareGrafosService } from 'services/getMaregrafos.service';
 
-interface useGetMareGrafosQueryProps {}
+interface useGetMareGrafosQueryProps {
+  isEnabled?: boolean;
+}
 
-export const useGetMareGrafosQuery = ({}: useGetMareGrafosQueryProps) => {
+export const useGetMareGrafosQuery = ({ isEnabled = false }: useGetMareGrafosQueryProps) => {
   return useQuery({
     queryKey: [QUERY_KEYS.maregrafosData],
     queryFn: () => getMareGrafosService(),
-    // enabled: false,
+    enabled: isEnabled,
     staleTime: 1000 * 60 * 1440, // 24 hours
     refetchOnWindowFocus: false,
     refetchOnMount: false,
